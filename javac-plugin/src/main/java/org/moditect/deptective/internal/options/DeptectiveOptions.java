15b92eef29a19fcfe000042048d7fd237d22cbe9
Andres Almiray <aalmiray@gmail.com>
2021-02-28 16:11:50 +0100

    Setup parent pom from ModiTect

--- javac-plugin/src/main/java/org/moditect/deptective/internal/options/DeptectiveOptions.java
+++ javac-plugin/src/main/java/org/moditect/deptective/internal/options/DeptectiveOptions.java
@@ -1,17 +1,19 @@
-/**
- *  Copyright 2019 The ModiTect authors
+/*
+ * SPDX-License-Identifier: Apache-2.0
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright 2019 The ModiTect authors.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.moditect.deptective.internal.options;
 
@@ -31,9 +33,11 @@ import org.moditect.deptective.internal.model.PackagePattern;
 /**
  * The options supported by the Deptective plug-in. To be given as javac plugin arguments through the
  * command line.
+ * 
  * <pre>
  *     -Xplugin:Deptective arg1=val1 ... argN=valN
  * </pre>
+ * 
  * @author Gunnar Morling
  */
 public class DeptectiveOptions {
